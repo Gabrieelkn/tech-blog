27305d65e91d90ca318fc6cfb8d71f8e1884e57a
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-04-22 23:34:15 +0300

    add filter by tags

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react";
 
 export default function SearchBar({ articles, setFilteredArticles }) {
   const [query, setQuery] = useState("");
-
   useEffect(() => {
     const filtered = articles.filter((article) => {
-      if (!query || article.title.toLowerCase().includes(query.toLowerCase())) {
-        return true;
-      }
-      return;
+      const articleTags = article.tags.map((tag) => tag.toLowerCase());
+      const hasMatchingTitle =
+        !query || article.title.toLowerCase().includes(query.toLowerCase());
+      const hasMatchingTag = articleTags.some((tag) =>
+        tag.includes(query.toLowerCase())
+      );
+      return hasMatchingTitle || hasMatchingTag;
     });
     setFilteredArticles(filtered);
   }, [articles, query, setFilteredArticles]);
@@ -17,7 +19,7 @@ export default function SearchBar({ articles, setFilteredArticles }) {
   return (
     <div className={styles.search}>
       <input
-        placeholder="Quick search..."
+        placeholder="Quick search by title or post tags..."
         type="text"
         onChange={(e) => setQuery(e.target.value)}
       />
