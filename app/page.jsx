125ad12c5007691def8c6c58d3caa4657704ad9f
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-04-27 22:03:38 +0300

    sort posts after fetching

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,15 @@ import { createClient } from "@/utils/supabaseServer";
 
 export default async function Home() {
   const supabase = createClient();
-  let { data: articles, error } = await supabase.from("articles").select("*");
+  let { data: articlesData, error } = await supabase
+    .from("articles")
+    .select("*");
+  const articles = articlesData.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+
+    return dateB - dateA;
+  });
 
   return (
     <div className={styles.home}>
